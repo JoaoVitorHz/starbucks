3f0567b6deaf191e9e822e26a8a73b351badbd27
João Vitor <vitorjoao39207@gmail.com>
2022-03-27 16:36:24 -0300

    Iniciando JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const btnGreenCoffe = document.querySelectorAll("greenCoffe");
+const btnPinkCoffe = document.querySelectorAll("pinkCoffe");
+const btnPurpleCoffe = document.querySelectorAll("purpleCoffe");
+
+btnGreenCoffe.addEventListener("click", function{
+    
+});
