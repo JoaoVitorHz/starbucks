3c4bd372ea7f70c214179aa012baf504daf481bf
João Vitor <vitorjoao39207@gmail.com>
2022-03-27 16:59:07 -0300

    Selecinado elementos de eventos de click

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # starbucks
 Copia de um site que eu achei no YouTube
+
+Botôes JavaScritp 
+
+GreenCoffe
+1ªRemover qualquer outro atributo de mudança de cor
+
+2ªEle tem que adicionar o atributo de mudança de cor verde
+- Adicionar um atributo a div "bola verde" 
+- Adicionar um atributo ao texto "STARBUKS"
+- Adicionar um atributo ao botão "Learn More"
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,14 @@ const btnGreenCoffe = document.querySelectorAll("greenCoffe");
 const btnPinkCoffe = document.querySelectorAll("pinkCoffe");
 const btnPurpleCoffe = document.querySelectorAll("purpleCoffe");
 
-btnGreenCoffe.addEventListener("click", function{
-    
+btnGreenCoffe.addEventListener("click", function green(){
+    alert("Julia ama Verde")
+});
+
+btnPinkCoffe.addEventListener("click", function pink() {
+    alert("Julia ama Rosa")
+});
+
+btnPurpleCoffe.addEventListener("click", function purple() {
+    alert("Julia ama Roxo")
 });
